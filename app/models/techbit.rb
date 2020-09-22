206812b380be2a259d0a578dafdf9b9a05ef46eb
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-09-22 18:04:44 +0100

    Setup association for followings

--- app/models/techbit.rb
+++ app/models/techbit.rb
@@ -1,4 +1,6 @@
 class Techbit < ApplicationRecord
+    validates :bit, presence: true
+    validates :author_id, presence: true
 
     belongs_to :author, class_name: :User, foreign_key: :author_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+    validates :full_name, presence: true
+    validates :username, presence: true
 
     has_many :techbits, foreign_key: :author_id
+    has_one_attached :photo
+    has_one_attached :coverimage
+
+    has_many :followed_users, foreign_key: :follower_id, class_name: 'Following'
+    has_many :followeds, through: :followed_users
+    has_many :following_users, foreign_key: :followed_id, class_name: 'Following'
+    has_many :followers, through: :following_users
 end

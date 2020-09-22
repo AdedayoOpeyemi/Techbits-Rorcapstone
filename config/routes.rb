1546b82cefbdc805a13aab3d00045571d79af64f
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-09-22 21:42:50 +0100

    Added sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :techbits
+  resources :users except: [:index, :new]
+  resources :techbits except: [:destroy, :edit, :update]
 
   root "techbits#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/users/:id/follow', to: "users#follow", as: "follow_user"
+  post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
 end

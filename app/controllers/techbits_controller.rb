3b7c77c652598a68de3e5c8a8724521295df217b
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-10-29 19:16:55 +0100

    Fixed styling error

--- app/controllers/techbits_controller.rb
+++ app/controllers/techbits_controller.rb
@@ -75,6 +75,7 @@ class TechbitsController < ApplicationController
   end
 
   private
+  
   # Use callbacks to share common setup or constraints between actions.
   def set_techbit
     @techbit = Techbit.find(params[:id])
--- app/helpers/techbits_helper.rb
+++ app/helpers/techbits_helper.rb
@@ -1,11 +1,2 @@
 module TechbitsHelper
-  def home_feed
-    # feed = logged_user.followeds + [logged_user]
-    Techbit.includes(:author).where(author_id: logged_user.followeds).or(Techbit.includes(:author).where(author_id: logged_user))
-    Techbit.includes(:author).where('author_id in (?)', (logged_user.followeds.id + [@user.id]))
-  end
-
-  def profile_bits(user)
-    user.techbits.all
-  end
 end
